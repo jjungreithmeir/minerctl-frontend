6b3cb1e204c278021edda57f607a26addfd3d47b
Jakob Jungreithmeir <jakob.jungreithmeir@bittex.at>
2018-06-14 10:27:15 +0200

    fix /settings page

--- src/json_parser.py
+++ src/json_parser.py
@@ -2,11 +2,14 @@ import requests
 import json
 
 container_conn = 'http://localhost:12345'
+sess = requests.Session()
+adapter = requests.adapters.HTTPAdapter(max_retries=10)
+sess.mount('http://', adapter)
 
 def parse_json(resource='/cfg'):
-    return requests.get(container_conn + resource).json()
+    return sess.get(container_conn + resource).json()
 
-def post_json(list):
+def post_json(list, resource='/cfg'):
     """
     Returns True if the POST action has been executed successfully.
     """
@@ -16,7 +19,10 @@ def post_json(list):
     copy.pop('action', None)
     copy.pop('file', None)
 
-    r = requests.post(container_conn, data=copy)
+    # TODO remove
+    print(copy)
+
+    r = sess.put(container_conn + resource, data=copy)
     return r.raise_for_status()
 
 def resp_to_dict(resp):
